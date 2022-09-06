52f4ecc82e9d0c6f8d292e8976afbe1ea18b658b
Dm4rt <danmartinez2004@gmail.com>
2022-09-06 07:01:12 -0400

    finished task B

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funcs.h"
+int main(){
+  print_interval(-2, 5);
+  std::cout<<"\n";
+  print_interval(3,12);
+  std::cout<<"\n";
+  print_interval(-10,4);
+  std::cout<<"\n";
+  
+}
